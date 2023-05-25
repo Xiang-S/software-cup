d62ce8cd1f63d9cf710d8fe1761bbaf64fce20a1
LinLai <zzbsn.cn@outlook.com>
2023-05-25 16:12:45 +0800

    队列页面添加BackTop 返回顶部功能

--- front-end/mobile/primary-ui/pages/common/woami.vue
+++ front-end/mobile/primary-ui/pages/common/woami.vue
@@ -1,11 +1,10 @@
 <template>
-	
 	<view class="content">
-		<text class="title">{{title}}</text>
-		<text class="title">{{message}}</text>
-		
+		<text class="title">{{ title }}</text>
+		<text class="title">{{ message }}</text>
+		<u-back-top :scroll-top="scrollTop"></u-back-top>
+
 	</view>
-	
 </template>
 
 
@@ -13,19 +12,22 @@
 	export default {
 		data() {
 			return {
-				title: '提交的队列 可以查询当前进度',
-				message: '查看历史记录',
-				disabled: false
-
-			}
+				scrollTop: 0,
+				title: "提交的队列 可以查询当前进度",
+				message: "查看历史记录",
+				disabled: false,
+			};
 		},
-		onLoad() {
-
+		onPageScroll(e) {
+			this.scrollTop = e.scrollTop;
 		},
+
+		onLoad() { },
+
 		methods: {
 
-		}
-	}
+		},
+	};
 </script>
 
 <style>
@@ -44,7 +46,6 @@
 		margin-right: auto;
 		margin-bottom: 50rpx;
 	}
-	
 
 	.text-area {
 		display: flex;
@@ -54,9 +55,10 @@
 	.title {
 		font-size: 36rpx;
 		color: $u-primary;
-
 	}
-
+	.wrap {
+		height: 200vh;
+	}
 	.swiper-item {
 		display: block;
 		height: 300rpx;
